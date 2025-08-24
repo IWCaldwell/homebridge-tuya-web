a67898b383bcfa35e08ddefb41148356b77354e4
Ian Caldwell <44706131+IWCaldwell@users.noreply.github.com>
2025-08-24 18:22:32 -0400

    Delete dist directory

--- dist/src/api/platform.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TuyaPlatforms = void 0;
-exports.TuyaPlatforms = ["tuya", "smart_life", "jinvoo_smart"];
-//# sourceMappingURL=platform.js.map
--- dist/src/helpers/TuyaBoolean.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TuyaBoolean = void 0;
-const TuyaBoolean = (value) => {
-    return String(value).toLowerCase() === "true";
-};
-exports.TuyaBoolean = TuyaBoolean;
-//# sourceMappingURL=TuyaBoolean.js.map
